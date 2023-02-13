f5e25f3292e40499826a064612aea5e7811d5bd0
Topi Toikka <topi.toikka@student.lab.fi>
2023-02-13 17:04:15 +0200

    fixed merge bugs

--- back/src/product-type/product-type.module.ts
+++ back/src/product-type/product-type.module.ts
@@ -1,13 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { ProductFamilyModule } from 'src/product-families/product-families.module';
-import { ProductFamilyService } from 'src/product-families/product-families.service';
-=======
->>>>>>> 3a911780995b8f137dc1e3c5b139e4d8fd1c334d
-=======
->>>>>>> f4011fafddd27963d1625008a062bd688fdab74d
 import { ProductType } from './entities/product-type.entity';
 import { ProductTypeController } from './product-type.controller';
 import { ProductTypeService } from './product-type.service';
@@ -19,6 +12,5 @@ import { ProductTypeService } from './product-type.service';
   controllers: [ProductTypeController],
   providers: [ProductTypeService],
   exports: [ProductTypeService],
-  imports: [TypeOrmModule.forFeature([ProductType])]
 })
 export class ProductTypeModule {}
