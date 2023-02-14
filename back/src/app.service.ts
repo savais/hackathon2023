e1dfe605d7463537512f8356f8c49076c8efa3cc
Topi Toikka <topi.toikka@student.lab.fi>
2023-02-14 14:51:17 +0200

    vikaa kommittia

--- back/src/app.service.ts
+++ back/src/app.service.ts
@@ -1,5 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { config } from "dotenv";
+import { Packet } from './packets/entities/packet.entity';
+import { PacketsService } from './packets/packets.service';
+import { ProductFamily } from './product-families/entities/product-family.entity';
+import { ProductFamilyService } from './product-families/product-families.service';
+import { CreateProductTypeDto } from './product-type/dto/create-product-type.dto';
+import { ProductType } from './product-type/entities/product-type.entity';
+import { ProductTypeService } from './product-type/product-type.service';
 
 type EnvKey = "JWT_SECRET" | "UPLOAD_DIR" | "PORT";
 
@@ -15,6 +22,14 @@ export function getEnv(key: EnvKey) {
 
 @Injectable()
 export class AppService {
+
+  constructor(
+    private packetService: PacketsService,
+    private productTypeService: ProductTypeService,
+    private productFamilyService: ProductFamilyService
+  ) {}
+
+
   getHello(): string {
     return 'Hello World!';
   }
