c076ffddaa24b75e1cfa4ee6c4817793114fe0df
Saku V <saku.vaisanen@student.lab.fi>
2023-02-15 17:46:25 +0200

    changed colors around a bit to be more in line with brand colors

--- front/src/app/upload-form/upload-form.module.ts
+++ front/src/app/upload-form/upload-form.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UploadFormComponent } from './upload-form.component';
 import { MaterialModule } from '../material.module';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [UploadFormComponent],
   imports: [
-    CommonModule, MaterialModule
+    CommonModule, MaterialModule, RouterModule
   ],
   exports: [
     UploadFormComponent
